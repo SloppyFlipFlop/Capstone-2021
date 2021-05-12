c90aee41c35b5fe946b86a9bab448b1e73db7973
David <david.nevarez03@hotmail.com>
2021-05-11 18:28:30 -0700

    added stuff

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
+
+// All Javascript was made by David Nevarez
+
 let data = {
     "lat": 33.53,
     "lon": -112.18,
@@ -1696,8 +1699,9 @@ let data = {
 
 // Get the Current Degrees, Time Zone, Feels Like
 
+// 16
 // Sun icon
-const sunnyIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-brightness-high" viewBox="0 0 16 16">
+const sunnyIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-brightness-high" viewBox="0 0 16 16">
 <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
 </svg>`
 
@@ -1720,7 +1724,7 @@ const NightAndCloudyIcon = `<i class="bi bi-cloud-moon"></i>`
 
 
 // An array of all the days in the week
-let daysInWeek = ["Sunday", "Monday", "Tuesday", "Wensday", "Thusday", "Friday", "saturday"]
+let daysInWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 function msToHMS(duration) {
 
@@ -1745,9 +1749,9 @@ for (let i = 0; i < data.daily.length; i++) {
 // console.log(dailyTempArray)
 
 const getDayType = (currentDay) => {
-    if(currentDay == "Clear") {
+    if (currentDay == "Clear") {
         return sunnyIcon;
-    }else {
+    } else {
         return "";
     }
 }
@@ -1765,10 +1769,24 @@ for (let i = 0; i <= 7; i++) {
 
     let dayIcon = data.daily[i].weather[0].main
 
-    console.log(day[i])
-    console.log(getDayType(dayIcon))
+    // console.log(day[i])
+    // console.log(getDayType(dayIcon))
 
     // Outputs the stuff to the div
-    dayelement.innerHTML = `${daysInWeek[i]}<br> ${getDayType(dayIcon)} <br> ${dailyTempArray[i]}`
+    dayelement.innerHTML = `<p class="title">${daysInWeek[i]}</p> <br> ${getDayType(dayIcon)} <br> <p>${dailyTempArray[i]}</p>`
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 }
+
+let dayArray = document.getElementsByClassName("weekday")
+
+
+// for (day in dayArray) {
+    dayArray.addEventListner("mouseover", (e) => {
+    console.log('test')
+    })
+// }
+
+
+
 
