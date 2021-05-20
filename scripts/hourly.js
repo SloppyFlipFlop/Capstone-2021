9b4ee937131fe0b818112e5f6b6deb292f127971
Fernando Nevarez <david.nevarez03@hotmail.com>
2021-05-20 11:52:12 -0700

    added hourly stuff

--- scripts/hourly.js
+++ scripts/hourly.js
@@ -129,10 +129,10 @@ Chart.defaults.global = {
     multiTooltipTemplate: "<%=datasetLabel%> : <%=value%>",
 
     // Function - Will fire on animation progression.
-    onAnimationProgress: function () { },
+    onAnimationProgress: function() {},
 
     // Function - Will fire on animation completion.
-    onAnimationComplete: function () { }
+    onAnimationComplete: function() {}
 };
 
 
@@ -143,46 +143,46 @@ Chart.defaults.global = {
 const lineGraphData = {
     labels: ["12AM", "1AM", "2AM", "3AM", "4AM", "5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM"],
     datasets: [{
-        label: "temperature( Fahrenheit )",
-        fillColor: "rgba(220,220,220,0.2)",
-        strokeColor: "rgba(220,220,220,1)",
-        pointColor: "rgba(220,220,220,1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(220,220,220,1)",
-        // goings to be thats days temp
-        data: []
-    },
-    {
-        label: "Humidity",
-        fillColor: "rgba(151,187,205,0.2)",
-        strokeColor: "rgba(151,187,205,1)",
-        pointColor: "rgba(151,187,205,1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(151,187,205,1)",
-        data: []
-    },
-    {
-        label: "Dew Point",
-        fillColor: "rgba(179, 229, 255, 0.2)",
-        strokeColor: "rgba(179, 229, 255, 1)",
-        pointColor: "rgba(179, 229, 255, 1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(179, 229, 255, 1)",
-        data: []
-    },
-    {
-        label: "Wind Speed ",
-        fillColor: "rgba(42, 143, 180, .2)",
-        strokeColor: "rgba(42, 143, 180, 1)",
-        pointColor: "rgba(42, 143, 180, 1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(42, 143, 180, 1)",
-        data: []
-    }
+            label: "temperature( Fahrenheit )",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            // goings to be thats days temp
+            data: []
+        },
+        {
+            label: "Humidity",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: []
+        },
+        {
+            label: "Dew Point",
+            fillColor: "rgba(179, 229, 255, 0.2)",
+            strokeColor: "rgba(179, 229, 255, 1)",
+            pointColor: "rgba(179, 229, 255, 1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(179, 229, 255, 1)",
+            data: []
+        },
+        {
+            label: "Wind Speed ",
+            fillColor: "rgba(42, 143, 180, .2)",
+            strokeColor: "rgba(42, 143, 180, 1)",
+            pointColor: "rgba(42, 143, 180, 1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(42, 143, 180, 1)",
+            data: []
+        }
     ]
 };
 
@@ -239,7 +239,7 @@ const lineGraphOptions = {
 for (let i = 0; i < 24; i++) {
     // Gets the temp of the hour
     let hoursTemp = data.hourly[i].temp
-    // This is where the data for the temp is going to get pushed
+        // This is where the data for the temp is going to get pushed
     let tempDataArea = lineGraphData.datasets[0].data;
     // Pushes the data to the place
     tempDataArea.push(hoursTemp)
@@ -257,7 +257,7 @@ for (let i = 0; i < 24; i++) {
     // ===============================================================
     // gets the dew point of each hour
     let hoursDewPoint = data.hourly[i].dew_point
-    // This is where the data for the dew point will get pushed
+        // This is where the data for the dew point will get pushed
     let dewDataArea = lineGraphData.datasets[2].data
 
     dewDataArea.push(hoursDewPoint)
@@ -315,19 +315,19 @@ function formatDuring(time) {
 
 // An array of all the temp
 const tempArray = []
-// An array of all the time data;
+    // An array of all the time data;
 const timeArray = []
-// An array of all the humidity data
+    // An array of all the humidity data
 const humidityArray = []
-// An array of all the dew Point data
+    // An array of all the dew Point data
 const dewPointArray = []
-// An array of all the presure data;
+    // An array of all the presure data;
 const cloudArray = []
-// An array of all the UV Inedx data;
+    // An array of all the UV Inedx data;
 const uvIndexArray = [];
 // An array of all the wind speed data;
 const windSpeedArray = []
-// An array of all the presure dat
+    // An array of all the presure dat
 const pressureArray = []
 
 // Gets each hour
@@ -349,7 +349,7 @@ for (let i = 0; i < 48; i++) {
     // Gets the Wind speed of the hour
     let windSpeed = data.hourly[i].wind_speed;
     // Get the clouds of the hour
-    let clouds =  data.hourly[i].clouds
+    let clouds = data.hourly[i].clouds
 
     // Pushing all the data to their array's
     tempArray.push(temp)
@@ -420,7 +420,7 @@ const dayTwoPressureArray = pressureArray.splice(-pressureHalf)
 
 // An array of all the day one's
 let allDayOneArray = []
-// An array of all the day two's
+    // An array of all the day two's
 let allDayTwoArray = []
 
 // I push all the data of each day into the big array for me to easily get it later
@@ -433,29 +433,29 @@ console.table(allDayTwoArray)
 
 // Goes through all the data in day one and starts to display it
 for (let i = 0; i < 24; i++) {
-    var parent = document.getElementById("accordionWrapper")
+    var parent = document.getElementById("accordionOneWrapper")
     parent.innerHTML += `
-    <div class="accordion" id="accordionExample">
+    <div class="accordion" id="accordionExample" style="margin-top: 5px">
             <div class="accordion-item">
                 <h2 class="accordion-header" id="headingOne">
                     <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        data-bs-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}" id="accordionHeader">
                         <div class="row w-100">
                             <div class="col-3">Time: ${allDayOneArray[0][i]}</div>
-                            <div class="col-3">Temp: ${allDayOneArray[1][i]}</div>
-                            <div class="col-3">Humidity: ${allDayOneArray[2][i]}</div>
-                            <div class="col-3">Dew Point: ${allDayOneArray[3][i]}</div>
+                            <div class="col-3">Temp: ${allDayOneArray[1][i]}<sup>&#8457;</sup></div>
+                            <div class="col-3">Humidity: ${allDayOneArray[2][i]}%</div>
+                            <div class="col-3">Dew Point: ${allDayOneArray[3][i]}&deg;</div>
                         </div>
                     </button>
                 </h2>
-                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+                <div id="collapse${i}" class="accordion-collapse collapse" aria-labelledby="headingOne"
                     data-bs-parent="#accordionExample">
                     <div class="accordion-body">
                         <div class="col-12">
                             <div class="row">
                                 <div class="col-6">
                                     <div class="row">
-                                        <div class="col-4 text-center pr-0 pl-4 justify-content-start d-flex">
+                                        <div class="col-4 text-center pr-0 pl-4 justify-content-start d-flex" style="padding: 0 !important;">
                                             <svg id="icons" xmlns="http://www.w3.org/2000/svg" width="16"
                                                 height="16" fill="currentColor" class="bi bi-arrow-bar-down"
                                                 viewBox="0 0 16 16">
@@ -469,7 +469,7 @@ for (let i = 0; i < 24; i++) {
                                 </div>
                                 <div class="col-6">
                                     <div class="row">
-                                        <div class="col-4 text-center">
+                                        <div class="col-4 text-center" style="width: fit-content;">
                                             <svg id="icons" xmlns="http://www.w3.org/2000/svg" width="16"
                                                 height="16" fill="currentColor" class="bi bi-sunglasses"
                                                 viewBox="0 0 16 16">
@@ -478,14 +478,14 @@ for (let i = 0; i < 24; i++) {
                                             </svg>
                                             UV Index
                                         </div>
-                                        <div class="col-8 d-flex justify-content-end">${allDayOneArray[5][i]}</div>
+                                        <div class="col-7 d-flex justify-content-end" style="margin-left: auto;">${allDayOneArray[5][i]} of 10</div>
                                     </div>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-6">
                                     <div class="row">
-                                        <div class="col-4 text-center p-0">
+                                        <div class="col-4 text-center p-0" style="width: fit-content">
                                             <svg id="icons" xmlns="http://www.w3.org/2000/svg" width="16"
                                                 height="16" fill="currentColor" class="bi bi-wind"
                                                 viewBox="0 0 16 16">
@@ -494,12 +494,12 @@ for (let i = 0; i < 24; i++) {
                                             </svg>
                                             Wind Speed
                                         </div>
-                                        <div class="col-8 d-flex justify-content-end p-0">${allDayOneArray[6][i]}</div>
+                                        <div class="col-7 d-flex justify-content-end p-0" style="margin-left: auto;">${allDayOneArray[6][i]}</div>
                                     </div>
                                 </div>
                                 <div class="col-6">
                                     <div class="row">
-                                        <div class="col-4 text-center">
+                                        <div class="col-4 ">
                                             <svg id="icons" xmlns="http://www.w3.org/2000/svg" width="16"
                                                 height="16" fill="currentColor" class="bi bi-cloud"
                                                 viewBox="0 0 16 16">
@@ -508,7 +508,93 @@ for (let i = 0; i < 24; i++) {
                                             </svg>
                                             Clouds
                                         </div>
-                                        <div class="col-8 d-flex justify-content-end">${allDayOneArray[4][i]}</div>
+                                        <div class="col-8 d-flex justify-content-end">${allDayOneArray[4][i]}%</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+}
+
+for (let i = 24; i < 48; i++) {
+    var parent = document.getElementById("accordionTwoWrapper")
+    parent.innerHTML += `
+    <div class="accordion" id="accordionExample" style="margin-top: 5px">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i - 24}" id="accordionHeader">
+                        <div class="row w-100">
+                            <div class="col-3">Time: ${allDayTwoArray[0][i - 24]}</div>
+                            <div class="col-3">Temp: ${allDayTwoArray[1][i - 24]}<sup>&#8457;</sup></div>
+                            <div class="col-3">Humidity: ${allDayTwoArray[2][i - 24]}%</div>
+                            <div class="col-3">Dew Point: ${allDayTwoArray[3][i - 24]}&deg;</div>
+                        </div>
+                    </button>
+                </h2>
+                <div id="collapse${i - 24}" class="accordion-collapse collapse" aria-labelledby="headingOne"
+                    data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="row">
+                                        <div class="col-4 text-center pr-0 pl-4 justify-content-start d-flex" style="padding: 0 !important;">
+                                            <svg id="icons" xmlns="http://www.w3.org/2000/svg" width="16"
+                                                height="16" fill="currentColor" class="bi bi-arrow-bar-down"
+                                                viewBox="0 0 16 16">
+                                                <path fill-rule="evenodd"
+                                                    d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6z" />
+                                            </svg>
+                                            Presure
+                                        </div>
+                                        <div class="col-8 pr-0 d-flex justify-content-end">${allDayTwoArray[7][i - 24]}</div>
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="row">
+                                        <div class="col-4 text-center" style="width: fit-content;">
+                                            <svg id="icons" xmlns="http://www.w3.org/2000/svg" width="16"
+                                                height="16" fill="currentColor" class="bi bi-sunglasses"
+                                                viewBox="0 0 16 16">
+                                                <path
+                                                    d="M3 5a2 2 0 0 0-2 2v.5H.5a.5.5 0 0 0 0 1H1V9a2 2 0 0 0 2 2h1a3 3 0 0 0 3-3 1 1 0 1 1 2 0 3 3 0 0 0 3 3h1a2 2 0 0 0 2-2v-.5h.5a.5.5 0 0 0 0-1H15V7a2 2 0 0 0-2-2h-2a2 2 0 0 0-1.888 1.338A1.99 1.99 0 0 0 8 6a1.99 1.99 0 0 0-1.112.338A2 2 0 0 0 5 5H3zm0 1h.941c.264 0 .348.356.112.474l-.457.228a2 2 0 0 0-.894.894l-.228.457C2.356 8.289 2 8.205 2 7.94V7a1 1 0 0 1 1-1z" />
+                                            </svg>
+                                            UV Index
+                                        </div>
+                                        <div class="col-7 d-flex justify-content-end" style="margin-left: auto;">${allDayTwoArray[5][i - 24]} of 10</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="row">
+                                        <div class="col-4 text-center p-0" style="width: fit-content">
+                                            <svg id="icons" xmlns="http://www.w3.org/2000/svg" width="16"
+                                                height="16" fill="currentColor" class="bi bi-wind"
+                                                viewBox="0 0 16 16">
+                                                <path
+                                                    d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5zm-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2zM0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5z" />
+                                            </svg>
+                                            Wind Speed
+                                        </div>
+                                        <div class="col-7 d-flex justify-content-end p-0" style="margin-left: auto;">${allDayTwoArray[6][i - 24]}</div>
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="row">
+                                        <div class="col-4 ">
+                                            <svg id="icons" xmlns="http://www.w3.org/2000/svg" width="16"
+                                                height="16" fill="currentColor" class="bi bi-cloud"
+                                                viewBox="0 0 16 16">
+                                                <path
+                                                    d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z" />
+                                            </svg>
+                                            Clouds
+                                        </div>
+                                        <div class="col-8 d-flex justify-content-end">${allDayTwoArray[4][i - 24]}%</div>
                                     </div>
                                 </div>
                             </div>
